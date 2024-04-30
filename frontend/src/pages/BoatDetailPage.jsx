9c9bf030f567ad950a975bfb70eaf6904e9efdd9
Zied1981 <zkhemiri@gmail.com>
2024-04-30 09:12:11 +0200

    done detailspage

--- frontend/src/pages/BoatDetailPage.jsx
+++ frontend/src/pages/BoatDetailPage.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { backendUrl } from "../api/api";
+
 const BoatDetailPage = () => {
+  const [boatDetails, setBoatDetails] = useState([]);
+  const { boatId } = useParams();
+
+  useEffect(() => {
+    fetch(`${backendUrl}/api/v1/boats/${boatId}`)
+      .then((res) => res.json())
+      .then((data) => setBoatDetails(data))
+      .catch((err) => console.log("Fail to fetch BoatDetails", err));
+  }, []);
+
+  console.log(boatDetails.name);
   return (
-    <section className=" bg-primary h-screen flex flex-col">
+    <section className=" flex flex-col">
+      <h1>Boat Detailpage</h1>
       <div>
         <img src="/img/placeholder-boat.jpg" alt="boat" />
-        <h1>Boat Detailpage</h1>
+        <h2>{boatDetails.name}</h2>
+        <p>BuiltYear:{boatDetails.builtYear}</p>
+        <p>Serialnumber:{boatDetails.serialnumber}</p>
+        <p>Material:{boatDetails.material}</p>
+        <p>Type:{boatDetails.type}</p>
+        <p>Capacity:{boatDetails.capacity}</p>
       </div>
     </section>
   );
